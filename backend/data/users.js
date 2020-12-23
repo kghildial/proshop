b83ccb631dff5c32f9cf5a7752d1656a1f4d8d42
Kinshuk Ghildial <kinshuk.ghildial@gmail.com>
2020-12-23 13:29:08 +0530

    Add seeder file to load the data in the DB

--- backend/data/users.js
+++ backend/data/users.js
@@ -9,13 +9,13 @@ const users = [
   },
   {
     name: 'John Doe',
-    email: 'admin@example.com',
+    email: 'johnDoe@example.com',
     password: bcrypt.hashSync('123456', 10),
     isAdmin: true,
   },
   {
     name: 'Jane Doe',
-    email: 'admin@example.com',
+    email: 'janeDoe@example.com',
     password: bcrypt.hashSync('123456', 10),
     isAdmin: true,
   },
